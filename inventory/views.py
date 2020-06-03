6ac01845caf01a3cc5d858fadb781e1b3a30f67b
cptc0ld <42217501+cptc0ld@users.noreply.github.com>
2020-06-03 15:01:31 +0530

    added flexbox to inventory

--- inventory/views.py
+++ inventory/views.py
@@ -14,47 +14,63 @@ def invo(request, steamid = None):
         j = response.json()
         context1 = list()
         context2 = list()
-        try:
-            for x in j['assets']:
-                assets = dict()
-                assets['steamid'] = steamid
-                assets['personaname'] = user.personaname
-                assets['profileurl'] = user.profileurl
-                assets['avatar'] = user.avatar
-                assets['avatarmedium'] = user.avatarmedium
-                assets['avatarfull'] = user.avatarfull
-                assets["appid"] = x["appid"]
-                assets["contextid"] = x["contextid"]
-                assets["assetid"] = x["assetid"]
-                assets["classid"] = x["classid"]
-                assets["instanceid"] = x["instanceid"]
-                assets["amount"] = x["amount"]
-                context2.append(assets)
-            for x in j['descriptions']:
-                # print(x)
-                description = dict()
-                description['appid'] = x['appid']
-                description['classid'] = x['classid']
-                description['instanceid'] = x['instanceid']
-                description['icon_url'] = x['icon_url']
-                description['name'] = x['name']
-                description['market_name'] = x['market_name']
-                description['tradable'] = x['tradable']
-                context1.append(description)
+    # try:
+        for x in j['assets']:
+            assets = dict()
+            assets['steamid'] = steamid
+            assets['personaname'] = user.personaname
+            assets['profileurl'] = user.profileurl
+            assets['avatar'] = user.avatar
+            assets['avatarmedium'] = user.avatarmedium
+            assets['avatarfull'] = user.avatarfull
+            assets["appid"] = x["appid"]
+            assets["contextid"] = x["contextid"]
+            assets["assetid"] = x["assetid"]
+            assets["classid"] = x["classid"]
+            assets["instanceid"] = x["instanceid"]
+            assets["amount"] = x["amount"]
+            context2.append(assets)
+        for x in j['descriptions']:
+            # print(x)
+            description = dict()
+            description['appid'] = x['appid']
+            description['classid'] = x['classid']
+            description['instanceid'] = x['instanceid']
+            description['icon_url'] = x['icon_url']
+            description['name'] = x['name']
+            description['market_name'] = x['market_name']
+            description['tradable'] = x['tradable']
+            description['items'] = list()
+            context1.append(description)
 
-            for x in context2:
-                for y in context1:
-                    if(x["classid"] == y["classid"]):
-                        x["icon_url"] = y["icon_url"]
-                        x["market_name"] = y["market_name"]
-                        x["tradable"] = y["tradable"]
+        for x in context1:
+            items = list()
+            for y in context2:
+                if(x["classid"] == y["classid"]):
 
-            content = {
-                'data' : context2
-            }
-            # print(len(context))
-            return render(request, 'inventory.html', content)
-        except:
-            return HttpResponse(str(response) + URL)
+                    x['steamid'] = y['steamid']
+                    x['personaname'] = y['personaname']
+                    x['profileurl'] = y['profileurl']
+                    x['avatar'] = y['avatar']
+                    x['avatarmedium'] = y['avatarmedium']
+                    x['avatarfull'] = y['avatarfull']
+                    x["contextid"] = y["contextid"]
+
+                    
+                    details = {'assetid' : y["assetid"], 'amount' : y["amount"]}
+                    items.append(details)
+                    x['items'] = items
+                    # x["assetid"] = y["assetid"]
+                    # x["amount"] = y["amount"]
+
+        context2.sort(key = lambda i: i['classid'])
+        # import pprint
+        # pprint.pprint(context1[70])
+        content = {
+            'data' : context1
+        }
+        return render(request, 'inventory.html', content)
+    # except:
+            # return HttpResponse(str(response) + URL)
     else:
         return redirect('auth:index')
